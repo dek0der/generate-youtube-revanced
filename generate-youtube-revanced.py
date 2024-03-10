d0e021d0b0405f8620e6269dbe8da42ce76f9b03
dek0der <63967935+dek0der@users.noreply.github.com>
2024-03-10 17:01:17 +0530

    Update generate-youtube-revanced.py
    Changed user agent to android

--- generate-youtube-revanced.py
+++ generate-youtube-revanced.py
@@ -96,7 +96,7 @@ def find_latest_supported_youtube_ver(url):
     return latest_ver
 
 def get_latest_supported_youtube_ver(yt_version):
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+    user_agent = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.3'
     headers = {'User-Agent' : user_agent}
     yt_version = yt_version.replace('.', '-')
     apkmirror_url = 'https://www.apkmirror.com'
